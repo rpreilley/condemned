d0b4ef670854924b4f5f97613bdecb12eb68a6da
Bob Reilley <rpreilley@gmail.com>
2017-01-13 13:55:25 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Condemned Escape Room Program Guide
+
+The main screen when opening is a text box and above is the background black box photo.
+
+Type in a code you believe will work during the event. Once code is typed in, that is a max of 8 characters, hit enter. If typing in one of the 5 correct codes, it will render that valve's reading. However, if the wrong code is typed in, it will render the incorrect image.
+
+Regardless of if code is correct or incorrect, after 3 seconds the image will reset to the blank black background.
+
+
+
+The code is set to refresh the browser automatically after the image disappears after 3 seconds, so no refresh is needed.
+
---
+++ script.js
@@ -0,0 +1,75 @@
+$( "#password" ).submit(function(event) {
+  event.preventDefault();
+  var value = this.password.value;
+  var check = value.toUpperCase();
+
+  if (check === "FUJWDBRY") {
+    valveOne();
+    this.password.value = '';
+  } else if (check === "ESGSUCKS") {
+    valveTwo();
+    this.password.value = '';
+  } else if (check === "URGNADIE") {
+    valveThree();
+    this.password.value = '';
+  }
+  else if (check === "IMDUMBAF") {
+    valveFour();
+    this.password.value = '';
+  }
+  else if (check === "THXSLASH") {
+    valveFive();
+    this.password.value = '';
+  } else if (check === "RICKROLL") {
+    rickRoll();
+    this.password.value = '';
+  } else {
+    wrongAnswer();
+    this.password.value = '';
+  }
+});
+
+
+function valveOne() {
+    var image = document.querySelectorAll("img")[0];
+    var source = image.src = image.src.replace("assets/gas_valve_system_background.gif","assets/gas_valve_system_correct1.gif");
+}
+
+function valveTwo() {
+    var image = document.querySelectorAll("img")[0];
+    var source = image.src = image.src.replace("assets/gas_valve_system_background.gif","assets/gas_valve_system_correct2.gif");
+}
+
+function valveThree() {
+    var image = document.querySelectorAll("img")[0];
+    var source = image.src = image.src.replace("assets/gas_valve_system_background.gif","assets/gas_valve_system_correct3.gif");
+}
+
+function valveFour() {
+    var image = document.querySelectorAll("img")[0];
+    var source = image.src = image.src.replace("assets/gas_valve_system_background.gif","assets/gas_valve_system_correct4.gif");
+}
+
+function valveFive() {
+    var image = document.querySelectorAll("img")[0];
+    var source = image.src = image.src.replace("assets/gas_valve_system_background.gif","assets/gas_valve_system_correct5.gif");
+}
+
+function wrongAnswer() {
+    var image = document.querySelectorAll("img")[0];
+    var source = image.src = image.src.replace("assets/gas_valve_system_background.gif","assets/gas_valve_system_incorrect.gif");
+}
+
+function rickRoll() {
+    var image = document.querySelectorAll("img")[0];
+    var source = image.src = image.src.replace("assets/gas_valve_system_background.gif","assets/rickroll.gif");
+}
+
+
+myVar = setTimeout(myTimer);
+function myTimer() {
+    var image = document.querySelectorAll("img")[0];
+    var source = image.src = "assets/gas_valve_system_background.gif";
+    clearTimeout(myVar);
+    Location.reload();
+}
