de44f875cc059c8768fdb9553003bfec8d1e53a1
Bob Reilley <rpreilley@gmail.com>
2017-01-16 10:31:20 -0600

    set validation for text input on keyup to be able to remove submit event handler

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Condemned Escape Room Program Guide
 
-The main screen when opening is a text box and above is the background black box photo.
+To run this program, open the folder including all of the files. Right click on the file called "index.html", then select to open with whatever is your default browser.
+
+The main screen when opening this file is a password entry box and above it is the background black box photo.
 
 Type in a code you believe will work during the event. Once code is typed in, that is a max of 8 characters, hit enter. If typing in one of the 5 correct codes, it will render that valve's reading. However, if the wrong code is typed in, it will render the incorrect image.
 
--- script.js
+++ script.js
@@ -1,33 +1,72 @@
-$( "#password" ).submit(function(event) {
+$("#password").on('keyup', function() {
   event.preventDefault();
   var value = this.password.value;
   var check = value.toUpperCase();
-
-  if (check === "FUJWDBRY") {
-    valveOne();
-    this.password.value = '';
-  } else if (check === "ESGSUCKS") {
-    valveTwo();
-    this.password.value = '';
-  } else if (check === "URGNADIE") {
-    valveThree();
-    this.password.value = '';
-  }
-  else if (check === "IMDUMBAF") {
-    valveFour();
-    this.password.value = '';
-  }
-  else if (check === "THXSLASH") {
-    valveFive();
-    this.password.value = '';
-  } else if (check === "RICKROLL") {
-    rickRoll();
-    this.password.value = '';
+  if (check.length === 8) {
+    if (check === "FUJWDBRY") {
+      valveOne();
+      this.password.value = '';
+    } else if (check === "ESGSUCKS") {
+      valveTwo();
+      this.password.value = '';
+    } else if (check === "URGNADIE") {
+      valveThree();
+      this.password.value = '';
+    }
+    else if (check === "IMDUMBAF") {
+      valveFour();
+      this.password.value = '';
+    }
+    else if (check === "THXSLASH") {
+      valveFive();
+      this.password.value = '';
+    } else if (check === "RICKROLL") {
+      rickRoll();
+      this.password.value = '';
+    } else {
+      wrongAnswer();
+      this.password.value = '';
+    }
   } else {
-    wrongAnswer();
-    this.password.value = '';
+    console.log('working');
   }
+
+  // if check.length === 8 {
+  //   console.log( check );
+  // }
 });
+
+
+// $( "#password" ).submit(function(event) {
+//   event.preventDefault();
+//   var value = this.password.value;
+//   var check = value.toUpperCase();
+
+//   if (check === "FUJWDBRY") {
+//     valveOne();
+//     this.password.value = '';
+//   } else if (check === "ESGSUCKS") {
+//     valveTwo();
+//     this.password.value = '';
+//   } else if (check === "URGNADIE") {
+//     valveThree();
+//     this.password.value = '';
+//   }
+//   else if (check === "IMDUMBAF") {
+//     valveFour();
+//     this.password.value = '';
+//   }
+//   else if (check === "THXSLASH") {
+//     valveFive();
+//     this.password.value = '';
+//   } else if (check === "RICKROLL") {
+//     rickRoll();
+//     this.password.value = '';
+//   } else {
+//     wrongAnswer();
+//     this.password.value = '';
+//   }
+// });
 
 
 function valveOne() {
@@ -71,5 +110,4 @@ function myTimer() {
     var image = document.querySelectorAll("img")[0];
     var source = image.src = "assets/gas_valve_system_background.gif";
     clearTimeout(myVar);
-    Location.reload();
 }
